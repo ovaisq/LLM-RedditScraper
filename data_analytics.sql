4c0fa019200f94c9c627661ef51bfa68e6a248a2
ovaisq <ovaisq@icloud.com>
2024-03-18 16:13:19 -0700

    update table names

--- data_analytics.sql
+++ data_analytics.sql
@@ -2,27 +2,27 @@
 SELECT post_author, subreddit
 FROM (
     SELECT post_author, subreddit
-    FROM post
+    FROM posts
     UNION
     SELECT comment_author as author_id, subreddit
-    FROM comment
+    FROM comments
 ) AS subreddits_by_author
 ORDER BY post_author, subreddit;
 --all posts and comments from the author
 SELECT
-    post.post_id AS post_id,
-    post.post_title AS post_title,
-    post.post_body AS post_body,
-    post.post_created_utc AS post_created_utc,
-    post.post_author AS post_author,
-    comment.comment_author AS comment_author,
-    comment.comment_id AS comment_id,
-    comment.comment_body AS comment_body,
-    comment.comment_created_utc AS comment_created_utc
+    posts.post_id AS post_id,
+    posts.post_title AS post_title,
+    posts.post_body AS post_body,
+    posts.post_created_utc AS post_created_utc,
+    posts.post_author AS post_author,
+    comments.comment_author AS comment_author,
+    comments.comment_id AS comment_id,
+    comments.comment_body AS comment_body,
+    comments.comment_created_utc AS comment_created_utc
 FROM
-    post
+    posts
 JOIN
-    comment ON post.post_author = comment.comment_author
+    comments ON posts.post_author = comments.comment_author
 WHERE
-    post.post_author = comment.comment_author
-    and post.post_body NOT IN ('[removed]', '');
+    posts.post_author = comments.comment_author
+    and posts.post_body NOT IN ('[removed]', '');
