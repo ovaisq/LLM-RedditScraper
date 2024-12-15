fe1b6ff6350edfd7eabe14000827e95f8f24a1bc
ovaisq <ovais@ifthenelse.org>
2024-12-15 01:16:32 -0800

    improved Jenkins Build Script

--- jenkins_build_script.sh
+++ jenkins_build_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Build package, then build docker image, push the docker image
-#  to private image registry running accessible on a local 
+#  to private image registry running accessible on a local
 #  network only
 #
 # IMPORTANT: Assumes that the node where this script will be run has
-#	"kubectl" installed and has deployment access to the destination
-#	kubernetes cluster
+#   "kubectl" installed and has deployment access to the destination
+#   kubernetes cluster
 #
 # Assumes that image reposity url is:
 #   docker-registry:5000
@@ -27,30 +27,58 @@ check_namespace() {
 }
 
 check_pod_status() {
-	namespace_name="$1"
-	while true 
-    do
-    	output=$(kubectl -n "${namespace_name}" get pods | grep rollama | head -n1 | awk '{print $3}')
-  
-  		if [ "$output" == "Running" ]
-        then
-    		break
-  		else
-    		echo "**** Output not yet 'Running', trying again..."
-    		sleep 2
-  		fi
-	done
+  namespace_name="$1"
+  iterations=0
+  while [ $iterations -lt 10 ]; do
+    output=$(kubectl -n "${namespace_name}" get pods | grep rollama | head -n1)
+    if [ "$output" == "Running" ]
+    then
+      echo "Pod is running."
+      return
+    elif [ $iterations -ge 10 ]; then
+      echo "Failed to start pod after 10 attempts"
+      exit 1 # hard exit with a non-zero status code
+    else
+      echo "**** Output not yet 'Running', trying again..."
+      sleep 2
+      ((iterations++))
+    fi
+  done
 }
+
+compare_strings() {
+  local recent_git_tag_commit_id="$1"
+  local recent_commit_id="$2"
+  local recent_git_tag="$3"
+
+  if [ "${recent_git_tag_commit_id}" = "${recent_commit_id}" ]
+  then
+    echo "Both Git Tag and Git Commit ID match"
+    export srvc_ver=$recent_git_tag
+  else
+    echo "Strings are not equal"
+    export srvc_ver=$(<ver.txt)
+  fi
+}
+
+echo "**** Setting Git user name and email"
+git config --global user.name "Jenkins Build"
+git config --global user.email "no-reply@ifthenelse.org"
+
+export curr_git_tag=$(git describe --abbrev=0)
+export curr_git_tag_commit_id=$(git rev-list -n 1 $curr_git_tag)
+export curr_commit_id=$(git rev-parse HEAD)
+
+compare_strings "${curr_git_tag_commit_id}" "${curr_commit_id}" "${curr_git_tag}"
 
 echo "**** Building Package"
 ./build.sh > /dev/null 2>&1
 
-export srvc_ver=$(<ver.txt)
+
 cd builds/BE_${srvc_ver}
 
 echo "**** Configuring Build"
-# change this to suit your needs
-scp localhost:/apps/rollama/setup.config .
+scp ovais@localhost:/home/ovais/rollama/setup.config .
 
 echo "**** Creating Docker image"
 ./build_docker.py > /dev/null 2>&1
@@ -59,8 +87,10 @@ echo "**** Clean up config"
 rm -f setup.config
 
 echo "**** Pushing Docker Image to Registry"
-docker tag rollama:${srvc_ver} dockeri-registry:5000/rollama:${srvc_ver} > /dev/null 2>&1
-docker push docker-registry:5000/rollama:${srvc_ver} > /dev/null 2>&1
+docker tag rollama:${srvc_ver} docker:5000/rollama:${srvc_ver} > /dev/null 2>&1
+docker tag rollama:latest docker:5000/rollama:latest > /dev/null 2>&1
+docker push docker:5000/rollama:${srvc_ver} > /dev/null 2>&1
+docker push docker:5000/rollama:latest > /dev/null 2>&1
 
 echo "**** Cleaning up orphaned images"
 # List orphaned images (those with the <none> tag)
@@ -94,6 +124,7 @@ then
         kubectl create namespace "${name_space}"
         echo "**** Deploying Rollama v${srvc_ver} to namespace [${name_space}]"
         echo "**** Deployment"
+        # deployment defaults to "latest" tag
         kubectl -n "${namespace}" apply -f deployment.yaml
         echo "**** Service"
         kubectl -n "${namespace}" apply -f service.yaml
@@ -119,8 +150,14 @@ running_srvc_ver=$(curl -X GET -s "http://k8prod-1.ifthenelse.org:${k8_srvc_node
 if [ -n "${running_srvc_ver}" ]
 then
 	echo "**** Running service version: ${running_srvc_ver}"
+    if [ "${curr_git_tag_commit_id}" != "${curr_commit_id}" ]
+    then
+    	export new_tag=$(<ver.txt)
+    	echo "Applying and pushing New Tag ${new_tag}"
+    	./git_tag_push.sh
+    fi
     exit 0
 else
-	echo "**** Service is not running"
+	echo "ERROR:**** Service is not running"
     exit -1
 fi
