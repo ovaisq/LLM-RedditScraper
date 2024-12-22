ba2e3cb8452f62d04002f3558e76aa5fc9632aa3
Jenkins Build <no-reply@ifthenelse.org>
2024-12-22 01:07:11 -0800

    debugging sed substitution issue

--- jenkins_build_script.sh
+++ jenkins_build_script.sh
@@ -129,7 +129,7 @@ then
     # deployment defaults to "latest" tag
     kubectl -n "${namespace}" apply -f deployment.yaml
     echo "**** Service v${srvc_ver}"
-		sed -i '' "s/SEMVER/${srvc_ver}/g" service.yaml
+	sed -i '' 's/SEMVER/'${srvc_ver}'/g' service.yaml
     kubectl -n "${namespace}" apply -f service.yaml
     check_pod_status "${namespace}"
     echo "**** Pod is now Running"
